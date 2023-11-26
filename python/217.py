fbaadfd89a87fa408c76c4269de0662d4b70c146
GiGi <gigi@hurtault.dev>
2023-11-26 00:25:03 -0500

    improve 217 python solution

--- python/217.py
+++ python/217.py
@@ -1,9 +1,16 @@
 nums = [1,2,3,4,5,5]
 
 def contains_duplicate(nums: [int]) -> bool:
+    already_seen = set()
+    
     for n in nums:
-        if nums.count(n) > 1:
+        #if nums.count(n) > 1:
+        if n in already_seen:
             return True
+        already_seen.add(n)
+
     return False
 
 print(contains_duplicate(nums))
+
+
