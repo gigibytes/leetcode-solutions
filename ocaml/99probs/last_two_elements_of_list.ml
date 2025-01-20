f5abe266fb8bad72b1bc411c8fb3fb5e2e5dac1f
GiGi Hurtault <gigi@hurtault.dev>
2025-01-20 13:28:34 -0500

    correct problem 2

--- ocaml/99probs/last_two_elements_of_list.ml
+++ ocaml/99probs/last_two_elements_of_list.ml
@@ -3,7 +3,8 @@ let nums = [1; 2; 3; 4; 5]
 let rec last_two l =
   match l with
   | [] -> None
-  | [ x; xs ] -> Some [ x; xs ]
+  (* A tuple seems better here *)
+  | [ x; xs ] -> Some (x, xs)
   | _ :: tl -> last_two tl
 
 let _ =
