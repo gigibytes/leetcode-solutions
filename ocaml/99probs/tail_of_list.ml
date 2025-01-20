e2c37599800f99ca7cb0bbc5dc3bb3fde781d6ff
GiGi Hurtault <gigi@hurtault.dev>
2025-01-20 13:19:34 -0500

    correct base case return types and add notes

--- ocaml/99probs/tail_of_list.ml
+++ ocaml/99probs/tail_of_list.ml
@@ -2,8 +2,11 @@ let nums = [1; 2; 3; 4; 5]
 
 let rec last l =
   match l with
-  | [] -> []
-  | [hd] -> hd
+  (* I had '[] -> []', it should return None instead *)
+  | [] -> None
+  (* This also needs to change to Some because we're returning None in the first case *)
+  (* the return type of the function becomes the option type *)
+  | [ x ] -> Some x
   | _ :: tl -> last tl
 
 let _ =
