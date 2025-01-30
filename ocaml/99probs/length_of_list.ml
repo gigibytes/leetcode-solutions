e1d175c792b648c25d3f8be5745890a6fb6d97a0
GiGi Hurtault <gigi@hurtault.dev>
2025-01-29 22:31:00 -0500

    add another length sol

--- ocaml/99probs/length_of_list.ml
+++ ocaml/99probs/length_of_list.ml
@@ -4,3 +4,8 @@ let rec count lst acc =
   | hd :: tl -> count tl (acc + 1)
 
 let length lst = count lst 0
+
+(* adding this scanning/non-recursive implementation -- though i bet it's recursive under the hood *)
+let count_fold lst =
+  let ones = List.map (fun _ -> 1) lst in
+  List.fold_right (fun x y -> x + y) ones 0
