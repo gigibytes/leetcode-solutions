6350c42140634b1f1e69c57d0ca4f61f8c227b27
gigi <5151947+gigibytes@users.noreply.github.com>
2025-02-21 23:58:58 -0500

    Update 2022-1.ml

--- ocaml/aoc/2022-1.ml
+++ ocaml/aoc/2022-1.ml
@@ -1,16 +1,16 @@
-(* incomplete. parsing is a bit off. need two newljnes at end for some reason. *)
+(* incomplete. parsing is a bit off. need newline to get last num for some reason. *)
 let cals = 
-{|"
+{|
 1234
 5678
 
 1000
 
-2222"|} in
+2222|} in
 let cals_list = String.split_on_char '\n' cals in
 let rec split_calories cal_list acc_sub acc =
   match cal_list with
-  | hd :: tl when hd != "" && hd != "\\"-> split_calories tl (hd :: acc_sub) acc
+  | hd :: tl when hd != "" -> split_calories tl (hd :: acc_sub) acc
   | hd :: tl -> split_calories tl [] (acc_sub :: acc)
   | [] -> acc
 in
