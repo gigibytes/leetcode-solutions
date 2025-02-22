f525451bb7a6ef8000ac393bf32ca448e9ad5909
gigi <5151947+gigibytes@users.noreply.github.com>
2025-02-21 23:48:19 -0500

    Create 2022-1.ml

---
+++ ocaml/aoc/2022-1.ml
@@ -0,0 +1,18 @@
+(* incomplete. parsing is a bit off. need two newljnes at end for some reason. *)
+let cals = 
+{|"
+1234
+5678
+
+1000
+
+2222"|} in
+let cals_list = String.split_on_char '\n' cals in
+let rec split_calories cal_list acc_sub acc =
+  match cal_list with
+  | hd :: tl when hd != "" && hd != "\\"-> split_calories tl (hd :: acc_sub) acc
+  | hd :: tl -> split_calories tl [] (acc_sub :: acc)
+  | [] -> acc
+in
+split_calories cals_list [] []
+...
