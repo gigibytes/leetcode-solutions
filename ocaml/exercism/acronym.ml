c9f9c99cf01080a9c9f8e9837a6615c772e58993
gigi <5151947+gigibytes@users.noreply.github.com>
2025-02-25 22:13:11 -0500

    Update acronym.ml

--- ocaml/exercism/acronym.ml
+++ ocaml/exercism/acronym.ml
@@ -1,5 +1,16 @@
 let acronym words =
-  let words_no_hyphen = String.split_on_char '-' words |> List.fold_left (^) "" in    
-  let punct = ['!'; '_'; '.'; ','; '@'; '#'; '$'; '['; ']'; '{'; '*'; '&'; '%'; '^'; ';'; '('; ')' ] in
-  let word_list = String.split_on_char ' ' words_no_hyphen in
-  word_list |> List.map (fun w -> List.init (String.length w) (String.get w) |> List.find (fun c -> List.mem c punct |> Bool.not) |> Char.uppercase_ascii) |> List.to_seq |> String.of_seq
+  let punct = ['!'; '_'; '.'; ','; '@'; '#'; '$'; '['; ']'; '{'; '*'; '&'; '%'; '^'; ';'; '('; ')' ]
+  in
+  let word_list = String.split_on_char ' ' words
+  in
+  let remove_punct c = function
+  | c when List.mem c punct -> '_'
+  | c when c = '-' -> ' '
+  | c -> c
+  in
+  let new_list = (List.map (fun w -> List.init (String.length w) (String.get w) |> remove_punct) word_list)
+  in
+  let rec get_first_char lst = function
+  | c when c = ' ' -> ""
+  | c -> String.make 1 c
+  in List.fold_left get_first_char "" new_list
